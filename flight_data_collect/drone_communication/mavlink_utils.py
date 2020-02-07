27d02910559c2d84b5ba5144088167bdb7ec565d
Lyuyang Hu <lyuyangh@uci.edu>
2020-02-07 11:12:51 -0800

    revert provide user with nofix info, need to refactor code, will provide in future

--- flight_data_collect/drone_communication/mavlink_utils.py
+++ flight_data_collect/drone_communication/mavlink_utils.py
@@ -38,8 +38,6 @@ def _log_latest_location(mavlink, drone_id):
         Location_log.objects.create(timestamp = datetime.now(), \
             latitude=global_position_int.lat/10**7, longitude=global_position_int.lon/10**7, \
             altitude=global_position_int.alt, heading=global_position_int.hdg, droneid=drone_id)
-    elif gps_raw and gps_raw.fix_type < mavlink_constants.GPS_2D_FIX:
-        send_message_to_clients("WARNING: NO GPS or NO FIX")
 
 
 def _get_mavlink_message(mavlink, message_name)->dict:
